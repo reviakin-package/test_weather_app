afd4e22690e8e6da5058d3c74842e33483f3a098
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-07-07 02:23:51 +0400

    Сreate WeatherApi.kt
    Creating an api interface for getting the weather

---
+++ app/src/main/java/com/example/test_weather/weatherapi/WeatherApi.kt
@@ -0,0 +1,11 @@
+package com.example.test_weather
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("data/2.5/weather?&lang=ru&units=metric&appid=0d95f07a988ae1b9467cd7c7c54f67cc")
+    fun getWeatherOnZipCode(@Query("q") zipCode: Int): Call<WeatherInfo>
+}
