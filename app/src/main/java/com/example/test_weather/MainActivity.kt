d6bea8727ee9e2962b201f12bfb707942510cdfc
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-07-07 02:17:20 +0400

    init commit
    initial app

---
+++ app/src/main/java/com/example/test_weather/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.test_weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
