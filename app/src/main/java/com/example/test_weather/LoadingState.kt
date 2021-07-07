127c30610161976724096a0477afcd93530e0f60
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-07-08 00:13:12 +0400

    Create LoadingState.kt
    Create helper class LoadingState

---
+++ app/src/main/java/com/example/test_weather/LoadingState.kt
@@ -0,0 +1,19 @@
+package com.example.test_weather
+
+import android.provider.VoicemailContract
+
+data class LoadingState private constructor(
+    val status: Status,
+    val msg: String? = null){
+    companion object{
+        val LOADED = LoadingState(Status.SUCCESS)
+        val LOADING = LoadingState(Status.RUNNING)
+        fun error(msg: String) = LoadingState(Status.FAILED, msg)
+    }
+
+    enum class Status{
+        RUNNING,
+        SUCCESS,
+        FAILED
+    }
+}
