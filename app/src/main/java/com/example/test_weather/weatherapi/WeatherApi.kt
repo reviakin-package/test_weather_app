6bf00473527a86b40d76c23ee17a2bc90448510d
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-07-08 03:16:49 +0400

    Update WeatherApi.kt

--- app/src/main/java/com/example/test_weather/weatherapi/WeatherApi.kt
+++ app/src/main/java/com/example/test_weather/weatherapi/WeatherApi.kt
@@ -1,5 +1,6 @@
 package com.example.test_weather
 
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,5 +8,6 @@ import retrofit2.http.Query
 interface WeatherApi {
 
     @GET("data/2.5/weather?&lang=ru&units=metric&appid=0d95f07a988ae1b9467cd7c7c54f67cc")
-    fun getWeatherOnZipCode(@Query("q") zipCode: Int): Call<WeatherInfo>
+    fun getWeatherOnZipCode(@Query("q") zipCode: Int): Deferred<WeatherInfo>
+
 }
