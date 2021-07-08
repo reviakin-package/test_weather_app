f73750f90d275a8891cc446779c8b761cac0412c
reviakin-package <80742342+reviakin-package@users.noreply.github.com>
2021-07-08 14:12:06 +0400

    Create MainActivity
    Create MainActivity.kt,
    Update language WeatherApi.kt

---
+++ app/src/main/java/com/example/test_weather/activity/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.test_weather.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.example.test_weather.R
+import com.example.test_weather.database.WeatherRow
+import com.example.test_weather.helper.LoadingState
+import com.example.test_weather.viewmodel.WeatherViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val weatherViewModel: WeatherViewModel by viewModels()
+
+    private lateinit var containerInfo: LinearLayout
+    private lateinit var editZipCode: EditText
+    private lateinit var btnGetWeather: Button
+    private lateinit var textLocation: TextView
+    private lateinit var textTemperature: TextView
+    private lateinit var textWindSpeed: TextView
+    private lateinit var textHumidity: TextView
+    private lateinit var textVisibility: TextView
+    private lateinit var textSunrise: TextView
+    private lateinit var textSunset: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+        initDataObservers()
+    }
+
+    fun initDataObservers(){
+        weatherViewModel.data.observe(this,
+            object : Observer<WeatherRow>{
+                override fun onChanged(t: WeatherRow?) {
+                    if(t != null) {
+                        setData(
+                            t?.location,
+                            t?.tempterature.toString(),
+                            t?.windSpeed.toString(),
+                            t?.humidity.toString(),
+                            t?.visibility,
+                            t?.sunsirse.toString(),
+                            t?.sunset.toString()
+                        )
+
+                        containerInfo.visibility = View.VISIBLE
+                    }
+                }
+
+            })
+
+        weatherViewModel.loadingState.observe(this,
+            object : Observer<LoadingState>{
+                override fun onChanged(t: LoadingState?) {
+                    when(t?.status){
+                        LoadingState.Status.FAILED -> {
+                            Toast.makeText(this@MainActivity, "Нет подключения к интернету", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+
+            })
+    }
+
+    fun setData(
+        location: String,
+        temperature: String,
+        windSpeed: String,
+        humidity: String,
+        visibility: String,
+        sunrise: String,
+        sunset: String
+    ){
+        textLocation.text = location
+        textTemperature.text = "$temperature C"
+        textWindSpeed.text = "$windSpeed mph"
+        textHumidity.text = "$humidity %"
+        textVisibility.text = visibility
+        textSunrise.text = sunrise
+        textSunset.text = sunset
+    }
+
+    fun initView(){
+        containerInfo = findViewById(R.id.containerInfo)
+        editZipCode = findViewById(R.id.editZipCode)
+        btnGetWeather = findViewById(R.id.btnGetWeather)
+        textLocation = findViewById(R.id.textLocation)
+        textTemperature = findViewById(R.id.textTemperature)
+        textWindSpeed = findViewById(R.id.textWindSpeed)
+        textHumidity = findViewById(R.id.textHumidity)
+        textVisibility = findViewById(R.id.textVisibility)
+        textSunrise = findViewById(R.id.textSunrise)
+        textSunset = findViewById(R.id.textSunset)
+        btnGetWeather.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnGetWeather -> {
+                weatherViewModel.fetchData(editZipCode.text.toString())
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/test_weather/weatherapi/WeatherApi.kt
+++ app/src/main/java/com/example/test_weather/weatherapi/WeatherApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface WeatherApi {
 
-    @GET("data/2.5/weather?&lang=ru&units=metric&appid=0d95f07a988ae1b9467cd7c7c54f67cc")
+    @GET("data/2.5/weather?&lang=en&units=metric&appid=0d95f07a988ae1b9467cd7c7c54f67cc")
     fun getWeatherOnZipCode(@Query("q") zipCode: Int): Deferred<WeatherInfo>
 
 }
